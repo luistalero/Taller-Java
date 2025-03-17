6dc31c3498cd4a12a7dd1fcf465c3b0714c6d024
luistalero <luisalbertotaleromartinez@gmail.com>
2025-03-17 16:20:19 -0500

    feat: :sparkles: Commit 3: Primer reto Propuesto finalizado

--- src/main/java/com/taller/Main.java
+++ src/main/java/com/taller/Main.java
@@ -3,6 +3,9 @@ package com.taller;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taller.equipos.Equip;
 import com.taller.equipos.EquiposLista;
@@ -22,7 +25,16 @@ public class Main {
             EquiposLista listaEquipos = om.readValue(is, EquiposLista.class);
             List<Equip> equipos = listaEquipos.equipos; 
       
-             
+            Predicate<Equip> fundado2000 = equipo -> equipo.yearfoundation < 2000;
+            List<Equip> filtro = equipos.stream().filter(fundado2000).collect(Collectors.toList());
+            System.out.println("\nEquipos fundados antes del año 2000");
+            if (filtro.isEmpty()) {
+              System.out.println("No hay equipos fundados antes del año 2000");
+            } else {
+              filtro.forEach(e -> 
+              System.out.println(e.name + "fundado en el año: " + e.yearfoundation)
+              );
+            }
       
         } catch (IOException e) {
             e.printStackTrace();
