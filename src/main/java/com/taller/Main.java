550144b3cadb0d8ead6068a1e6b511de21a57fca
luistalero <luisalbertotaleromartinez@gmail.com>
2025-03-18 13:26:40 -0500

    Feat: :memo: Commit 4:

--- src/main/java/com/taller/Main.java
+++ src/main/java/com/taller/Main.java
@@ -3,13 +3,16 @@ package com.taller;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.OptionalDouble;
 //// import java.util.function.Consumer;
 //// import java.util.function.Predicate;
 //// import java.util.stream.Collectors;
+import java.util.function.BiConsumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.taller.equipos.Equip;
 import com.taller.equipos.EquiposLista;
+import com.taller.equipos.Jugador;
 
 
 
@@ -18,15 +21,16 @@ public class Main {
         try {
 
             InputStream is = Main.class.getClassLoader().getResourceAsStream("uefa_teams_36_full_players.json");
-      
+    
             if (is == null) {
-              throw new RuntimeException("No Se Encontró el archivo JSON en resources.");
+                throw new RuntimeException("No Se Encontró el archivo JSON en resources.");
             }
             ObjectMapper om = new ObjectMapper();
             EquiposLista listaEquipos = om.readValue(is, EquiposLista.class);
             List<Equip> equipos = listaEquipos.equipos; 
-      
+    
             //Inicio de Primer Reto
+
             //// Predicate<Equip> fundado2000 = equipo -> equipo.yearfoundation > 2000;
             //// List<Equip> filtro = equipos.stream().filter(fundado2000).collect(Collectors.toList());
             //// System.out.println("\nEquipos fundados Después del año 2000");
@@ -47,9 +51,39 @@ public class Main {
             // Final del Segundo Reto
 
             // Inicio Tercer Reto
-            
+            BiConsumer<Equip, List<Jugador>> promedioEdad = (equipo, jugadores) -> {OptionalDouble promedio = jugadores.stream().mapToInt(jugador -> jugador.age).average();
+                System.out.println(equipo.name + " - Promedio de Edad: " + (promedio.));
+            }
             // Final Tercer Reto
-      
+
+            // Inicio Cuarto Reto
+            
+            // Final Cuarto Reto
+
+            // Inicio Quinto Reto
+            
+            // Final Quinto Reto
+
+            // Inicio Sexto Reto
+            
+            // Final Sexto Reto
+
+            // Inicio Septimo Reto
+            
+            // Final Septimo Reto
+
+            // Inicio Octavo Reto
+            
+            // Final Octavo Reto
+
+            // Inicio Noveno Reto
+            
+            // Final Noveno Reto
+
+            // Inicio Decímo Reto
+            
+            // Final Decímo Reto
+    
         } catch (IOException e) {
             e.printStackTrace();
         }
